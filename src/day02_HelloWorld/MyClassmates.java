20a24126ac9273f872d8fbefcb60d490c8c4a2bd
Banu <banu2106@gmail.com>
2023-02-24 16:01:48 +0300

    d

--- src/day02_HelloWorld/MyClassmates.java
+++ src/day02_HelloWorld/MyClassmates.java
@@ -2,6 +2,11 @@ package day02_HelloWorld;
 
 public class MyClassmates {
     public static void main(String[] args) {
+        System.out.println("Betul");
+        System.out.println("Bahar");
+        System.out.println("Yusuf");
+        System.out.println("Yigit");
+        System.out.println("Sukru");
 
     }
 }
