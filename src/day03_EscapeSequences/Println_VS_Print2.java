0f17c20f61b2fdefd289a7380b738a8b1a6aedcc
Banu <banu2106@gmail.com>
2023-02-27 15:53:14 +0300

    d

---
+++ src/day03_EscapeSequences/Println_VS_Print2.java
@@ -0,0 +1,29 @@
+package day03_EscapeSequences;
+
+public class Println_VS_Print2 {
+    public static void main(String[] args) {
+        System.out.println("Knock knock");
+        System.out.println("Who is this?");
+
+        System.out.println("This is Java");
+        System.out.println("-----------------");
+
+        System.out.print("Knock knock");
+        System.out.print("Who is this?");
+        System.out.print("This is java");
+        System.out.println();
+
+
+        System.out.println("------------------");
+
+        System.out.println("Hello everyone, How are you all today? Today we will learn escape sequences and next week we will learn variables");
+
+        System.out.println("-------------------");
+
+        System.out.print("Hello everyone, How are you all today?");
+        System.out.print("Today we will learn escape sequences");
+        System.out.print("and next week we will learn variables");
+
+
+    }
+}
