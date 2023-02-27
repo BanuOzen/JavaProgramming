6f7636c7ef141416d016ae37f8d051d60a022eef
Banu <banu2106@gmail.com>
2023-02-27 16:23:01 +0300

    d

---
+++ src/day03_EscapeSequences/EscapeSequences.java
@@ -0,0 +1,32 @@
+package day03_EscapeSequences;
+/*
+escape sequences: must be given with double quote
+\n:start the new line
+\t:paragraph space(tab)
+\\:single back slash
+\":double quote
+
+ */
+
+public class EscapeSequences {
+    public static void main(String[] args) {
+        System.out.println("Java \n Python \n C#");
+
+        System.out.println("-----------------------------------------");
+
+        System.out.println("Hello Cydeo \nHow are you all today? \nIt's nice to see you all! \nWhat class do we have next week?");
+
+        System.out.println("-----------------------------------------");
+
+        System.out.println("\tJava is cool Programming Language");
+
+        System.out.println("---------------------------------------");
+        System.out.println("/ \\");// in order to print one backword slash we need to give two backword slash
+
+        System.out.println("------------------");
+        System.out.println("My favorite TV show \"is Game of Throne\"");
+
+
+
+    }
+}
