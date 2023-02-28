1718284c95e60d2f3c1942c9c2371a43fe22aa40
Banu <banu2106@gmail.com>
2023-02-28 23:34:09 +0300

    d

---
+++ src/day04_Variables/PrimitivesIntro.java
@@ -0,0 +1,26 @@
+package day04_Variables;
+
+public class PrimitivesIntro {
+    public static void main(String[] args) {
+        // age :38 years old
+        byte x = 38;
+        // weight: 160 pounds
+        //byte weight =160  // 160 is out of the byte range
+        // byte  num= -129;// -129 is out of byte range
+        short weight = 160; // 160 is within the range of short
+
+        //salary:100000$
+        //short salary=100000; 100000 is out short range
+        int salary = 100_000;//
+
+
+        long asset = 3_333_333_333l;
+        // tax:0.26
+        float tax = 0.26F;
+
+        double PI =3.14;
+
+
+
+    }
+}
