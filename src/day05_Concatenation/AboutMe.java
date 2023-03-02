029b5d269dc05d0ba2ce2db51e722e2ee3e7a5fe
Banu <banu2106@gmail.com>
2023-03-02 23:57:39 +0300

    d

---
+++ src/day05_Concatenation/AboutMe.java
@@ -0,0 +1,13 @@
+package day05_Concatenation;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        String name = "banu";
+        int age = 35;
+        // Hi, My name is ______, and I am ___ years old.
+
+        System.out.println("Hi, My name is " + name + ", and I am " + age + " years old.");
+
+
+    }
+}
