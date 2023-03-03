1eea160e41eccf729621058cfba4641f8127f336
Banu <banu2106@gmail.com>
2023-03-03 11:15:17 +0300

    d

---
+++ src/day05_Concatenation/FullName.java
@@ -0,0 +1,23 @@
+package day05_Concatenation;
+
+public class FullName {
+    public static void main(String[] args) {
+        String firstName = "Cydeo";
+        String lastName = "School";
+        int age = 55;
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(fullName);
+
+        // Full name of the person is ________
+        System.out.println("Full name of the person is ");
+        // ____ is ___ years old
+        System.out.println(fullName + " is " + age + " years old ");
+
+        // FullName is JobTitle, works at CompanyName, and salary is Salary
+
+
+
+
+    }
+}
