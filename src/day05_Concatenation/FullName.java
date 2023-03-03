4082d8af7df2c272209aeab89e0cc2b8cdf7fe22
Banu <banu2106@gmail.com>
2023-03-03 11:26:39 +0300

    d

--- src/day05_Concatenation/FullName.java
+++ src/day05_Concatenation/FullName.java
@@ -2,9 +2,13 @@ package day05_Concatenation;
 
 public class FullName {
     public static void main(String[] args) {
-        String firstName = "Cydeo";
-        String lastName = "School";
-        int age = 55;
+        String firstName = "Aeron";
+        String lastName = "Daniel";
+        int age = 20;
+        String jobTitle = "SDET";
+        String companyName = "Apple Inc";
+        double salary = 100000.58;
+
         String fullName = firstName + " " + lastName;
 
         System.out.println(fullName);
@@ -15,7 +19,7 @@ public class FullName {
         System.out.println(fullName + " is " + age + " years old ");
 
         // FullName is JobTitle, works at CompanyName, and salary is Salary
-
+        System.out.println(fullName + " is " + jobTitle + " works at " + companyName + ", and " + fullName + "'s salary is $" + salary);
 
 
 
