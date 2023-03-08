7d8912b10240ea1596ca2821a67fd288d91e09ef
Banu <banu2106@gmail.com>
2023-03-08 23:17:35 +0300

    day08-IfStatement

---
+++ src/day08_IfStatements/SingleIfStatement.java
@@ -0,0 +1,45 @@
+package day08_IfStatements;
+
+public class SingleIfStatement {
+    public static void main(String[] args) {
+        int number = 300;
+
+        /*System.out.println("Odd Number");
+
+        System.out.println("Even Number");
+
+         */
+
+        boolean evenNumber = number%2==0;
+        boolean oddNumber = !evenNumber; // not even number
+
+        if(evenNumber){ // even number
+            System.out.println(number + " is even number");
+        }
+        if(oddNumber){
+            System.out.println(number + " is odd number");
+        }
+        System.out.println("-----------------------------");
+
+        int n = 200;
+
+        //positive
+        if(n>0){
+            System.out.println(n + " is positive");
+        }
+        //negative
+        if(n<0){// if n is less than zero, then it's negative
+            System.out.println(n + " is negative");
+        }
+        // zero
+        if(n==0){
+            System.out.println(n + " is zero");
+        }
+
+
+
+
+
+
+    }
+}
