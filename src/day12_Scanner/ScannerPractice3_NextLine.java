ec5722d106909bb885a2563e91dc5855d70c1a8c
Banu <banu2106@gmail.com>
2023-03-15 16:12:44 +0300

    day-12-Scanner

--- src/day12_Scanner/ScannerPractice3_NextLine.java
+++ src/day12_Scanner/ScannerPractice3_NextLine.java
@@ -1,7 +1,32 @@
 package day12_Scanner;
 
+import java.util.Scanner;
+
 public class ScannerPractice3_NextLine {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your full name: ");
+        String fullName = input.nextLine();
+
+        System.out.println("Enter your Programming Language");
+        String programmingLanguage = input.nextLine();
+
+        System.out.println("Enter your age: ");
+        int age = input.nextInt();
+
+        input.nextLine(); // captue the entre key that user pressed for nextInt()
+        System.out.println("Enter your school name: ");
+        String schoolName = input.nextLine();
+
+
+        System.out.println("fullName = " + fullName);
+        System.out.println("programmingLanguage = " + programmingLanguage);
+        System.out.println("age = " + age);
+        System.out.println("schoolName = " + schoolName);
+        input.close();
+
 
     }
 }
