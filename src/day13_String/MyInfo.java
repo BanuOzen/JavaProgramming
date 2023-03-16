e97accb519ced74b5b7a0f9883ba5e902f4a8ecb
Banu <banu2106@gmail.com>
2023-03-16 23:05:12 +0300

    day-13-String

--- src/day13_String/MyInfo.java
+++ src/day13_String/MyInfo.java
@@ -15,8 +15,48 @@ public class MyInfo {
         String gender=scan.next();
 
         scan.nextLine();
+
         System.out.println("Enter your full name: " );
         String fullName=scan.nextLine();
+
+        System.out.println("Enter your phone number: ");
+        long phoneNumber= scan.nextLong();
+
+        System.out.println("Enter your zip code: ");
+        int zipCode=scan.nextInt();
+
+        scan.nextLine();
+
+        System.out.println("Enter your school name: ");
+        String schoolName=scan.nextLine();
+
+        System.out.println("Enter your city name: " );
+        String cityName= scan.nextLine();
+
+        System.out.println("Enter your state name: ");
+        String stateName=scan.next();
+
+        System.out.println("Enter your building number: " );
+        int buildingNumber= scan.nextInt();
+
+        scan.nextLine();
+        System.out.println("Enter your street name: ");
+        String streetName=scan.nextLine();
+
+        scan.close();
+
+        System.out.println("fullName = " + fullName);
+        System.out.println("age = " + age);
+        System.out.println("gender = " + gender);
+        System.out.println("phoneNumber = " + phoneNumber);
+        System.out.println("Address: \n\t" + buildingNumber + " " + streetName + "\n\t " + cityName +","
+        + stateName +" " + zipCode);
+        System.out.println("schoolName = " + schoolName);
+
+
+
+
+
 
 
     }
@@ -33,5 +73,18 @@ public class MyInfo {
 			8. Enter your state name (String- One word ONLY)------>next
 			9. Enter your building number (int)------>nextInt
 			10. Enter your Street name----->nextLine
+			MAKE SURE USER CAN ENTER ALL THE INPUT
+
+		Display all the inputs that user entered in following order in sperate lines:
+
+			1. full name
+			2. age
+			3. gender
+			4. phone number
+			5. address:
+						buildingNumber Street
+						City, State ZipCode
+
+			6. school name
 
  */
