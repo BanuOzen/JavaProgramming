637f72752ace56a8d2585b46b7192ea7f8e6b1e9
Banu <banu2106@gmail.com>
2023-03-18 09:38:49 +0300

    day-14-String

--- src/day14_String/StringMethods1.java
+++ src/day14_String/StringMethods1.java
@@ -35,11 +35,26 @@ public class StringMethods1 {
         int lastA = s.lastIndexOf("a");
         int secondA = s.indexOf("a ");
         int thirdA = s.indexOf("a C");
+        //int fourthA = s.lastIndexOf("av");
+       // int fourthA = s.indexOf("ava W");
+        int fourthA =s.indexOf("Ca") +1;
+        //int fifthA = s.lastIndexOf("va") + 1;
+        int fifthA= s.lastIndexOf("a W");
+        int sixthA = s.lastIndexOf("aw");
+        //int seventhA=s.indexOf("a o");
+        int seventhA = s.lastIndexOf("a ");
+
+
 
         System.out.println("firstA = " + firstA);
         System.out.println("lastA = " + lastA);
         System.out.println("secondA = " + secondA);
         System.out.println("thirdA = " + thirdA);
+        System.out.println("fourthA = " + fourthA);
+        System.out.println("fifthA = " + fifthA);
+        System.out.println("sixthA = " + sixthA);
+        System.out.println("seventhA = " + seventhA);
+
 
 
 
