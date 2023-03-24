828e730dbbbbb69631c4bbf8dbf1b981c5b0dfeb
Banu <banu2106@gmail.com>
2023-03-25 00:38:28 +0300

    day17-WhileLoopBranchingStatementsWhile&Do

---
+++ src/day17_While_DoWhile/WhileLoopPractice.java
@@ -0,0 +1,37 @@
+package day17_While_DoWhile;
+
+public class WhileLoopPractice {
+    public static void main(String[] args) {
+
+        //usernama:"Cydeo"
+        //password:"Cydeo123"
+
+        int attempt=3;
+
+        //while(invalid&& hasAttempts)
+
+
+
+
+
+
+
+
+
+
+    }
+}
+/*
+  2. You are writing a code for the log-in function of the Cybertek Application,
+   assume that your credentials are:
+                    username: Cydeo
+                    password: Cydeo123
+
+   Ask the user to enter their credentials.
+       If credentials are matched, your program should print "Logged in."
+       If the credentials are not matched, the program should allow the user to
+       have three attempts to enter correct credentials and
+       if all three attempts are failed, then print "Your account is locked."
+
+
+ */
