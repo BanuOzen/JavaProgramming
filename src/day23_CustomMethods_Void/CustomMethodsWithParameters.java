6ab5044617c164aeb38fa27d23d588677cd709af
Banu <banu2106@gmail.com>
2023-04-05 16:28:46 +0300

    day23_CustomMethods_Void

---
+++ src/day23_CustomMethods_Void/CustomMethodsWithParameters.java
@@ -0,0 +1,24 @@
+package day23_CustomMethods_Void;
+
+public class CustomMethodsWithParameters {
+
+
+
+
+
+
+    // create a function that can check if a number is odd number or even number
+    public static void oddOrEven(int number){
+
+        if(number % 2 == 0){
+            System.out.println(number + " is even number");
+        }else{
+            System.out.println(number + " is odd number");
+        }
+
+
+
+    }
+
+
+}
