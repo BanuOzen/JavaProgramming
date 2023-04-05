ee8ef6547457e134db404c697ecb74d4e0ae0dce
Banu <banu2106@gmail.com>
2023-04-05 16:19:57 +0300

    day23_CustomMethods_Void

--- src/day23_CustomMethods_Void/CustomMethodsPractice.java
+++ src/day23_CustomMethods_Void/CustomMethodsPractice.java
@@ -1,10 +1,23 @@
 package day23_CustomMethods_Void;
 
+import javax.swing.plaf.synth.SynthFormattedTextFieldUI;
+
 public class CustomMethodsPractice {
     public static void main(String[] args) {
+
         helloWorld5Times();
+
+        System.out.println("--------------");
+
+        helloCydeo5Times();
+
+        System.out.println("----------------");
+
+        evenNumbers();
+
     }
-   // create a function that  can print hello world 5 times==> helloWorld5Times()
+
+    // create a function that  can print hello world 5 times==> helloWorld5Times()
 
     public static void helloWorld5Times(){
         for (int i = 0; i < 5; i++) {
@@ -14,6 +27,26 @@ public class CustomMethodsPractice {
     }
 
     // create a function that can print hello Cydeo 5 times==> helloCydeo5Times()
+
+    public static void helloCydeo5Times(){
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello Cydeo");
+        }
+
+    }
+
+    // create a function that can print all the even numbers from 1~10==> evenNumbers
+    public static void evenNumbers(){
+
+        for (int i = 2; i < 11; i+=2) {
+            System.out.println(i);
+        }
+
+
+    }
+
+
 
 
 
