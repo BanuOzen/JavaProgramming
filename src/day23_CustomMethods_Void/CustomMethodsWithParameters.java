bd360d23817e5e267b59455fe569456fa6067aec
Banu <banu2106@gmail.com>
2023-04-05 16:47:39 +0300

    day23_CustomMethods_Void

--- src/day23_CustomMethods_Void/CustomMethodsWithParameters.java
+++ src/day23_CustomMethods_Void/CustomMethodsWithParameters.java
@@ -2,7 +2,15 @@ package day23_CustomMethods_Void;
 
 public class CustomMethodsWithParameters {
 
+    public static void main(String[] args) {
 
+
+        //oddOrEven(); // the method demands additional info to complete its task
+
+        oddOrEven(10);
+
+        ageOfPerson(1994);
+    }
 
 
 
@@ -15,6 +23,18 @@ public class CustomMethodsWithParameters {
         }else{
             System.out.println(number + " is odd number");
         }
+
+
+
+    }
+
+    // create a function that can display the age of the person
+    public static void ageOfPerson(int birthYear){
+
+        int age = 2021 - birthYear;
+
+        System.out.println("Your age is: " + age);
+
 
 
 
