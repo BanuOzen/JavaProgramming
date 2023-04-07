c7f5031d404a0f2e308082cf3d3cafce78a873be
Banu <banu2106@gmail.com>
2023-04-07 13:27:45 +0300

    day24_CustomMethodReturn

--- src/day24_CustomMethodReturn/ReturnStatement.java
+++ src/day24_CustomMethodReturn/ReturnStatement.java
@@ -8,26 +8,25 @@ public class ReturnStatement {
     }
 
     public static void nameOfDay(int number){
-
         if(number < 1 || number > 7){ // if the number is invalid
             System.out.println("Invalid");
             return;  //exits nameOfDay method
         }
-        if(number == 1){
+            if(number == 1){
             System.out.println("Monday");
-        }else if(number==2){
+            }else if(number==2){
             System.out.println("Tuesday");
-        }else if(number==3){
+            }else if(number==3){
             System.out.println("Wednesday");
-        }else if(number==4){
+            }else if(number==4){
             System.out.println("Thursday");
-        }else if(number==5){
+            }else if(number==5){
             System.out.println("Friday");
-        }else if(number==6){
+            }else if(number==6){
             System.out.println("Saturday");
-        }else {
+            }else {
             System.out.println("Sunday");
-        }
+            }
 
-    }
+        }
 }
