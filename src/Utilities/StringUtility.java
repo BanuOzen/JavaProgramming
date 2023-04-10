2dfa2bd54e3f68e9a201d3cdb8cd5c58f74fdd19
Banu <banu2106@gmail.com>
2023-04-11 01:22:46 +0300

    day25_CustomMethod_Overloading

---
+++ src/Utilities/StringUtility.java
@@ -0,0 +1,15 @@
+package Utilities;
+
+public class StringUtility {
+
+   // prints each character of the given string
+    public static void printEachChar(String str){
+
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+        }
+
+    }
+
+
+}
