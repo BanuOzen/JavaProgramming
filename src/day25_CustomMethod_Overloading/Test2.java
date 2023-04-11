6a31519deef77d2f7300a915d868bb16eab36ddb
Banu <banu2106@gmail.com>
2023-04-11 13:24:33 +0300

    day25_CustomMethod_Overloading

---
+++ src/day25_CustomMethod_Overloading/Test2.java
@@ -0,0 +1,39 @@
+package day25_CustomMethod_Overloading;
+
+import Utilities.ArraysUtility;
+
+import java.util.Arrays;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {1,2,3,4};
+        ArraysUtility.printEachElement(arr1);
+
+
+        System.out.println("=======================");
+
+
+        double[] arr2 = {1.5,2.5,3.5,4.5};
+        ArraysUtility.printEachElement(arr2);
+
+
+        System.out.println("=========================");
+
+
+        char[] arr3 = {'A','B','C','D'};
+        ArraysUtility.printEachElement(arr3);
+
+
+        System.out.println("=========================");
+
+
+        String[] names = {"David", "Elvira", "Ali", "Igor"};
+        ArraysUtility.printEachElement(names);
+
+
+
+    }
+
+}
