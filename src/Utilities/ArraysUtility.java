d668192c187955036d4ff480d3376a014675aec4
Banu <banu2106@gmail.com>
2023-04-11 13:06:25 +0300

    day25_CustomMethod_Overloading

---
+++ src/Utilities/ArraysUtility.java
@@ -0,0 +1,44 @@
+package Utilities;
+
+public class ArraysUtility {
+
+    //prints each integer of an integer array in separate lines
+    public static void printEach(int[] array){
+
+        for (int each: array) {
+            System.out.println(each);
+        }
+
+    }
+
+    //prints each double of a double array in separate lines
+    public static void printEach(double[] array){
+
+        for (double each: array) {
+            System.out.println(each);
+        }
+
+    }
+
+    //prints each char of a char array in separate lines
+    public static void printEach(char[] array){
+
+        for (char each: array) {
+            System.out.println(each);
+        }
+
+    }
+
+
+    //prints each String of a String array in separate lines
+    public static void printEach(String[] array){
+
+        for (String each: array) {
+            System.out.println(each);
+        }
+
+    }
+
+
+
+}
