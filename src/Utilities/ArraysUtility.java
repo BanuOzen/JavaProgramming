2452bb6797101d0ef6dc70c33019825fcdd40bf1
Banu <banu2106@gmail.com>
2023-04-12 15:28:26 +0300

    day26_CustomMethodPractice

--- src/Utilities/ArraysUtility.java
+++ src/Utilities/ArraysUtility.java
@@ -228,6 +228,51 @@ public class ArraysUtility {
         return count;
 
     }
+    // returns the unique elements of the array as a new array
+    public static int[] uniqueElements(int[] array){
+        int[] result = {};
+        for (int each : array) {
+            if( ArraysUtility.frequencyOfElement(array,each) == 1){
+                // if the frequency is one, the element is unique
+                result = ArraysUtility.addElement(result,each);
+            }
+        }
+        return result;
+    }
+    // returns the unique elements of the array as a new array
+    public static double[] uniqueElements(double[] array){
+        double[] result = {};
+        for (double each : array) {
+            if( ArraysUtility.frequencyOfElement(array,each) == 1){
+                // if the frequency is one, the element is unique
+                result = ArraysUtility.addElement(result,each);
+            }
+        }
+        return result;
+    }
+
+    // returns the unique elements of the array as a new array
+    public static char[] uniqueElements(char[] array){
+        char[] result = {};
+        for (char each : array) {
+            if( ArraysUtility.frequencyOfElement(array,each) == 1){
+                // if the frequency is one, the element is unique
+                result = ArraysUtility.addElement(result,each);
+            }
+        }
+        return result;
+    }
+    // returns the unique elements of the array as a new array
+    public static String[] uniqueElements(String[] array){
+        String[] result = {};
+        for (String each : array) {
+            if( ArraysUtility.frequencyOfElement(array,each) == 1){
+                // if the frequency is one, the element is unique
+                result = ArraysUtility.addElement(result,each);
+            }
+        }
+        return result;
+    }
 
 
 }
