9b492a7fabd574e0cde10bb5ad10a17cf13b5847
Banu <banu2106@gmail.com>
2023-04-12 16:33:05 +0300

    day26_CustomMethodPractice

---
+++ src/day26_CustomMethodsPractice/RemoveElements2.java
@@ -0,0 +1,36 @@
+package day26_CustomMethodsPractice;
+
+import Utilities.ArraysUtility;
+
+import java.util.Arrays;
+
+public class RemoveElements2 {
+    public static void main(String[] args) {
+        int[] numbers = { 100, 200, 300, 400, 500, 600};
+
+        numbers = removeElement(numbers,1);//{100,300,400,500,600}
+        numbers = removeElement(numbers,1);
+        System.out.println(Arrays.toString(numbers));
+
+    }
+
+    // removes the index from the array, returns new array
+    public static int[] removeElement(int[] array, int index){
+
+        if(index < 0 || index > array.length-1){
+            System.err.println("Invalid Index Number: " + index);
+            System.exit(0);
+        }
+
+        int[] result={};
+
+        for (int i = 0; i < array.length; i++) {//i: array's index number
+            if( i != index){
+               result = ArraysUtility.addElement(result,array[i]);
+            }
+        }
+        return result;
+
+    }
+
+}
