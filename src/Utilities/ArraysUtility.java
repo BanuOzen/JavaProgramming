1c0f9c2ef3af90d30ba0decd1134a5338dd5f04a
Banu <banu2106@gmail.com>
2023-04-13 17:59:35 +0300

    day27_WrapperClasses

--- src/Utilities/ArraysUtility.java
+++ src/Utilities/ArraysUtility.java
@@ -578,6 +578,52 @@ public class ArraysUtility {
 
 
 
+    public static int[] removeDuplicates(int[] array){
+
+        int[] result = {};
+
+        for (int each : array) {
+            if(!ArraysUtility.contains(result,each)){
+                result = ArraysUtility.addElement(result, each);
+            }
+        }
+        return result;
+    }
+
+    public static double[] removeDuplicates(double[] array){
+
+        double[] result = {};
+
+        for (double each : array) {
+            if(!ArraysUtility.contains(result,each)){
+                result = ArraysUtility.addElement(result, each);
+            }
+        }
+        return result;
+    }
+
+    public static char[] removeDuplicates(char[] array){
+
+        char[] result = {};
+
+        for (char each : array) {
+            if(!ArraysUtility.contains(result,each)){
+                result = ArraysUtility.addElement(result, each);
+            }
+        }
+        return result;
+    }
+    public static String[] removeDuplicates(String[] array){
+
+        String[] result = {};
+
+        for (String each : array) {
+            if(!ArraysUtility.contains(result,each)){
+                result = ArraysUtility.addElement(result, each);
+            }
+        }
+        return result;
+    }
 
 
 
