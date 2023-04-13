a873446427733ad36b4f776c0cf82a4c4a411337
Banu <banu2106@gmail.com>
2023-04-13 22:11:17 +0300

    day27_WrapperClasses

---
+++ src/day27_WrapperClasses/WrapperClassMethods.java
@@ -0,0 +1,51 @@
+package day27_WrapperClasses;
+
+public class WrapperClassMethods {
+    public static void main(String[] args) {
+
+        String str = "123";
+        int num = Integer.parseInt(str); //int
+
+        System.out.println(num + 1); //124
+        System.out.println(str+1); //1231
+
+        String str2 = "10.5";
+        double num2 =Double.parseDouble(str2);
+
+        System.out.println(num2 + 1);
+
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
+
+        long max2 = Long.MAX_VALUE;
+        long min2 = Long.MIN_VALUE;
+
+        System.out.println("max2 = " + max2);
+        System.out.println("min2 = " + min2);
+
+        String s1 = "false";
+        boolean r1 = Boolean.parseBoolean(s1);
+        System.out.println(r1);
+
+        String s2 = "123";
+        Integer x = Integer.valueOf(s2);
+        int y = Integer.valueOf(s2);
+
+        System.out.println(x);
+        System.out.println(y);
+
+        String s3 = "1.5";
+        Double z = Double.valueOf(s3);
+
+        System.out.println(z);
+
+        System.out.println("-------------------------------");
+
+
+
+
+    }
+}
