a14e725cb890366b34f639c851e09a66c2326c8e
Banu <banu2106@gmail.com>
2023-04-16 02:07:06 +0300

    day28_ArrayList

--- src/day28_ArrayList/ArrayListMethods1.java
+++ src/day28_ArrayList/ArrayListMethods1.java
@@ -18,6 +18,41 @@ public class ArrayListMethods1 {
         numbers.add(5,45);
 
         System.out.println(numbers);
+        System.out.println( numbers.size() );
+        int lastIndex = numbers.size()-1;
+        System.out.println("lastIndex = " + lastIndex);
+
+        int num = numbers.get(3);
+        System.out.println("num = " + num);
+
+        System.out.println("==============================");
+
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.println( numbers.get(i) );
+        }
+
+        System.out.println("***********************");
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("Python");
+        list.add("Java");
+        list.add("C#");
+        list.add("Ruby");
+
+        list.set(2,"JavaScript");
+        list.set(3,"C++");
+
+        System.out.println(list);
+
+
+
+
+
+
+
+
+
 
 
     }
