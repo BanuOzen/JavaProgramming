e876b170ed18fdebe156b7dd66399ee1f445d419
Banu <banu2106@gmail.com>
2023-04-25 00:28:25 +0300

    day30_CustomClass

--- src/day30_CustomClass/DogObjects.java
+++ src/day30_CustomClass/DogObjects.java
@@ -1,5 +1,8 @@
 package day30_CustomClass;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class DogObjects {
 
     public static void main(String[] args) {
@@ -32,6 +35,27 @@ public class DogObjects {
         dog1.eat();
 
         dog2.bark();
+
+        Dog dog4 = new Dog();
+        dog4.setInfo("Bullet", "Labrador",3,'M', "Extra Large", "Yellow");
+        Dog dog5 = new Dog();
+        dog5.setInfo("Sully", "PitBull", 6, 'M', "Large", "Black");
+
+
+        Dog[] dogs = {dog1, dog2, dog3, dog4, dog5};
+        ArrayList<Dog> femaleDogs = new ArrayList<>();
+        femaleDogs.addAll(Arrays.asList(dogs));
+        femaleDogs.removeIf(p-> p.gender=='M');
+        System.out.println("These are femaleDogs : "+ femaleDogs.toString());
+
+
+        ArrayList<Dog> maleDogs = new ArrayList<>();
+        maleDogs.addAll(Arrays.asList(dogs));
+        maleDogs.removeIf(p-> p.gender=='F');
+        System.out.println("These are maleDogs : "+ maleDogs.toString());
+
+
+
 
 
 
