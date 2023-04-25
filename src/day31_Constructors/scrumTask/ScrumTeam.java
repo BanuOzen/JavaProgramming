3aebfbb8e84e7846b95ab954cf31a4ea02c1143d
Banu <banu2106@gmail.com>
2023-04-26 00:26:14 +0300

    day31-Constructors

--- src/day31_Constructors/scrumTask/ScrumTeam.java
+++ src/day31_Constructors/scrumTask/ScrumTeam.java
@@ -1,12 +1,13 @@
 package day31_Constructors.scrumTask;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ScrumTeam {
 
     public String PO, BA, SM;
     public ArrayList<Tester> testersList = new ArrayList<>();
-    public ArrayList<Developer> developerList = new ArrayList<>();
+    public ArrayList<Developer> devopsList = new ArrayList<>();
     public int daysOfSprint;
 
     public ScrumTeam(String PO, String BA, String SM, int daysOfSprint) {
@@ -15,6 +16,32 @@ public class ScrumTeam {
         this.SM = SM;
         this.daysOfSprint = daysOfSprint;
     }
+
+    public void addTester(Tester tester){
+        testersList.add( tester );
+    }
+
+    public void addTester(Tester[] testers){
+        testersList.addAll(Arrays.asList( testers ));
+    }
+
+
+    public void addDeveloper(Developer developer){
+        devopsList.add ( developer );
+    }
+
+    public void addDevelopers(Developer[] developers){
+        devopsList.addAll( Arrays.asList( developers ) );
+    }
+
+    public void removeTester(int employeeID){
+        testersList.removeIf( p-> p.employeeID == employeeID );
+    }
+    public void removeDeveloper(int employeeID){
+        devopsList.removeIf( p-> p.employeeID == employeeID);
+    }
+
+
 }
 /*
 create a class called ScrumTeam
