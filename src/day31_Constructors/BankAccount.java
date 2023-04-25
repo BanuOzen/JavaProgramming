5ca1fd2a0dfa1d2d9b85153506bd7955c2a2ec6b
Banu <banu2106@gmail.com>
2023-04-25 14:59:45 +0300

    day31-Constructors

---
+++ src/day31_Constructors/BankAccount.java
@@ -0,0 +1,22 @@
+package day31_Constructors;
+
+public class BankAccount {
+
+    public String accountHolder;
+    public long accountNumber;
+    public double balance;
+
+
+    public void setInfo(String accountHolder, long accountNumber, double balance){
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+    }
+
+    public String toString() {
+        return "BankAccount{" +
+                "accountHolder='" + accountHolder + '\'' +
+                ", accountNumber=" + accountNumber +
+                ", balance=" + balance +
+                '}';
+    }
+}
