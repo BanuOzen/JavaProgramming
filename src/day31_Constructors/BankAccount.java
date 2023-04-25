6cf298224a27145d0eced8596a3f12191e06e382
Banu <banu2106@gmail.com>
2023-04-25 15:43:19 +0300

    day31-Constructors

--- src/day31_Constructors/BankAccount.java
+++ src/day31_Constructors/BankAccount.java
@@ -19,4 +19,31 @@ public class BankAccount {
                 ", balance= $" + balance +
                 '}';
     }
+
+    public void checkBalance(){
+        System.out.println("Your available balance is: " + balance);
+    }
+
+    public void deposit(double amount){
+        if(amount < 0){
+            System.out.println("Depositing amount can not be zero or negative");
+            return;//exits the method
+        }
+        balance += amount;
+
+    }
+
+    public void withDraw(double amount){
+        if(amount > balance){// if withdrawing amount is greater than available balance
+            System.out.println("Insufficient balance");
+            return;
+        }
+        if(amount <= 0){ // if withdrawing amount is negative or zero
+            System.out.println("Withdrawing amount can not be negative or zero");
+            return;
+        }
+        balance -= amount;
+    }
+
+
 }
