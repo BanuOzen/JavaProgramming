d6d072f9c51ec95f176a4df0a3bc926dc33e341b
Banu <banu2106@gmail.com>
2023-04-29 14:26:46 +0300

    day34_GarbageCollection&AccessModifier

---
+++ src/Utilities/AccessModifiers_Test2.java
@@ -0,0 +1,17 @@
+package Utilities;
+
+import day34_GarbageCollection_AccessModifier.AccessModifiers;
+import day34_GarbageCollection_AccessModifier.AccessModifiers_Test;
+
+public class AccessModifiers_Test2 {
+    public static void main(String[] args) {
+
+        System.out.println( AccessModifiers.publicData );
+       // System.out.println( AccessModifiers.defaultData);// default Access Modifier is not visible outside the package
+       // System.out.println(AccessModifiers.privateData);// private Access Modifier is not visible outside the package
+
+
+
+    }
+
+}
