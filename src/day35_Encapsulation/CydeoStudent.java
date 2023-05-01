5eb6bbe66fe97b30c31a09f5c65e8787d5195ac8
Banu <banu2106@gmail.com>
2023-05-01 20:26:34 +0300

    day35-Encapsulation

---
+++ src/day35_Encapsulation/CydeoStudent.java
@@ -0,0 +1,53 @@
+package day35_Encapsulation;
+
+public class CydeoStudent {
+
+    public String name;
+    public char gender;
+    public int age;
+    public int batchNumber;
+    public int groupNumber;
+    public static String schoolName;
+    public String fieldOfStudy;
+    public static String programmingLanguage;
+    public static String secretCode;
+
+
+    public CydeoStudent(String name, char gender, int age, int batchNumber, int groupNumber, String fieldOfStudy) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.batchNumber = batchNumber;
+        this.groupNumber = groupNumber;
+        this.fieldOfStudy = fieldOfStudy;
+    }
+    static{
+        schoolName = "CYDEO";
+        programmingLanguage = "JAVA";
+        secretCode = "Wooden Spoon";
+    }
+
+
+
+
+
+
+
+
+}
+/*
+2. create a class named CybertekStudent
+            Variables:
+                name, gender, age, batchNumber, groupNumber, schoolName, fieldOfStudy, programmingLanguage, secretCode
+
+            Add a constructor to initialize all the fields
+
+            Add a static block to initialize all the statics
+
+            methods:
+                printSchoolName();
+                printSecretCode();
+                attendClass():
+                study()
+                toString()
+ */
