0149997bdc4b746e8bfe00650f1185fa1ebbd750
Banu <banu2106@gmail.com>
2023-05-02 17:11:05 +0300

    day36_Inheritance

--- src/day36_Inheritance/Encapsulation/Student.java
+++ src/day36_Inheritance/Encapsulation/Student.java
@@ -7,6 +7,13 @@ public class Student {
     private char gender, grade;
     private String schoolName;
 
+    public Student(String name, int age, char gender, char grade, String schoolName) {
+       setName(name);
+       setAge(age);
+       setGender(gender);
+       setGrade(grade);
+       setSchoolName(schoolName);
+    }
 
     public String getName(){
         return name;
@@ -22,7 +29,7 @@ public class Student {
 
     public void setAge(int age){
         if(age > 5 || age < 90){
-            return;
+            return; // exits the method
         }
         this.age = age;
     }
@@ -32,6 +39,10 @@ public class Student {
     }
 
     public void setGender(char gender) {
+        if( !(gender == 'M' || gender == 'F') ){
+            return;
+        }
+
         this.gender = gender;
     }
 
@@ -40,6 +51,10 @@ public class Student {
     }
 
     public void setGrade(char grade) {
+        if( !(grade == 'A' || grade == 'B' || grade == 'C' || grade == 'D' || grade == 'F') ){
+            return; // exits the method
+        }
+
         this.grade = grade;
     }
 
@@ -51,4 +66,16 @@ public class Student {
         this.schoolName = schoolName;
     }
 
+    public void study(){
+        System.out.println(name + " is studying ");
+    }
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", grade=" + grade +
+                ", schoolName='" + schoolName + '\'' +
+                '}';
+    }
 }
