6334b2880abf499be7b9ebad876ef85c5eb03e61
Banu <banu2106@gmail.com>
2023-05-13 13:36:27 +0300

    day42_ExceptionsContinue

---
+++ src/day42_Exceptions/ThrowsKeyword.java
@@ -0,0 +1,34 @@
+package day42_Exceptions;
+
+import java.io.FileNotFoundException;
+
+public class ThrowsKeyword {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("------------TEST1--------------");
+
+        System.out.println("Hello");
+
+            Thread.sleep(3000);
+
+        System.out.println("Cydeo");
+
+        System.out.println("----------Test1 Completed--------------");
+
+        System.out.println("-------------TEST2----------------");
+
+        System.out.println("Hello");
+
+            Thread.sleep(3000);
+
+        System.out.println("Batch 25");
+
+        System.out.println("-------------Test2 Completed--------------");
+
+
+
+
+
+
+    }
+
+}
