2b5f611ea1c3b5afb0ec779a81b4e1741852fb4b
Banu <banu2106@gmail.com>
2023-05-16 14:56:22 +0300

    day43_Abstraction

--- src/day42_Exceptions/MorningWorkout.java
+++ src/day42_Exceptions/MorningWorkout.java
@@ -28,10 +28,12 @@ public class MorningWorkout {
 
     }
     public static void sleep(double seconds){
+
         try {
             Thread.sleep((long) (seconds * 1000));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
     }
 }
