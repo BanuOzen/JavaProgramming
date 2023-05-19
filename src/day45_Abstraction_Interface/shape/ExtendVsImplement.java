f29875e7170d3e9c1aeff69be3f9db75ff9e9eff
Banu <banu2106@gmail.com>
2023-05-19 15:00:30 +0300

    day45_AbstractionContinue

---
+++ src/day45_Abstraction_Interface/shape/ExtendVsImplement.java
@@ -0,0 +1,35 @@
+package day45_Abstraction_Interface.shape;
+
+
+class C{
+
+}
+
+class A{
+
+}
+
+class B extends A{
+
+}
+interface Z{
+
+}
+
+interface X{
+
+}
+
+interface Y extends X,Z{
+
+}
+//implements
+
+class D implements X,Y,Z{
+
+}
+
+
+public class ExtendVsImplement {
+
+}
