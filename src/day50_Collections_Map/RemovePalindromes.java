9bd5cde394a42762b7bbfd915041c29c445f9dfb
Banu <banu2106@gmail.com>
2023-05-29 22:02:58 +0300

    day50_CollectionIterable

---
+++ src/day50_Collections_Map/RemovePalindromes.java
@@ -0,0 +1,21 @@
+package day50_Collections_Map;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RemovePalindromes {
+    public static void main(String[] args) {
+
+        List<String> names = new ArrayList<>();
+        names.addAll(Arrays.asList(
+           "Java", "Python", "Cydeo", "Car", "Level", "civic", "radar", "rotor", "kayak",
+                "reviver", "racecar", "madam"
+        ));
+
+
+
+
+
+    }
+}
