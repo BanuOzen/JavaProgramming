ac2cc1ec3f56f44d68e64c373cb15bb9f98163a3
Banu <banu2106@gmail.com>
2023-06-05 01:12:57 +0300

    day53_FunctionalInterface

---
+++ src/day53_FunctionalInterface/BuildFunctionalInterfaces1.java
@@ -0,0 +1,56 @@
+package day53_FunctionalInterface;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class BuildFunctionalInterfaces1 {
+    public static void main(String[] args) {
+
+        Predicate<String> isPalindrome = (p) -> {
+            String reverse = new StringBuilder(p).reverse().toString();
+            return reverse.equalsIgnoreCase(p);
+        };
+
+        boolean result1 = isPalindrome.test("Java");
+        System.out.println(result1);
+
+        Predicate<Integer> isEven = p -> p % 2 == 0;
+
+        System.out.println("===============================");
+
+        List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,1,2,3,4,5,6,7,1,2,3));
+
+        list.removeIf(p -> p % 2 == 0);
+
+        System.out.println(list);
+
+        System.out.println("=============================================");
+
+        List<String> names = new ArrayList<>();
+        names.addAll(Arrays.asList("Level", "Anna", "Java", "Python"));
+
+        names.removeIf(isPalindrome);
+        System.out.println(names);
+
+        System.out.println("====================================");
+
+        Consumer<String> printEach = (s) -> {
+            for (int i = 0; i < s.length(); i++) {
+                System.out.println(s.charAt(i));
+            }
+
+        };
+
+        printEach.accept("Java");
+
+        System.out.println("==================================");
+
+
+
+
+
+    }
+}
